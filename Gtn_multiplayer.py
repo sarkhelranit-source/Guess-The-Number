bffea9619f11837866d8cdbb3b719c559da01282
sarkhelranit-source <99674950+sarkhelranit-source@users.noreply.github.com>
2024-10-14 23:48:34 +0530

    Update Gtn_multiplayer.py

--- Gtn_multiplayer.py
+++ Gtn_multiplayer.py
@@ -1,36 +1,41 @@
 import random
-turn, super_turn, draw = 0, 0, 0
+turn, super_turn, draw, count = 0, 0, 0, 0
 turns, player_names, draw_index, tied_names = [], [], [], []
 
 players = int(input("How many players are there: "))
 print("")
 player_dict = {}
 
-for i in range(players):
-    if i == players - 1:
-        names = input(f"Name of player{i+1}: ")
-        print("")
+while (count < players):
+    
+    names = input(f"Name of player{count + 1}: ")
+
+    if names == "":
+        print("You must enter a name")
     else:
-        names = input(f"Name of player{i+1}: ")
-    player_names.append(names)
+        player_names.append(names)
+        count += 1
 
 while super_turn < players:
     random_number = random.randint(1, 101)
     while 1:
-        num = int(input(f"The computer selected a number between 1 to 100. {player_names[super_turn]} guess the number: "))
-        turn += 1
-        if num < random_number:
-            print(f"Try something bigger")
-        elif num > random_number:
-            print(f"Try something smaller")
-        else:
-            print(f"You took {turn} turns to guess it. great job!!\n============================================\n")
-            turns.append(turn)
-            turn = 0
-            super_turn += 1
-            for i in range(super_turn):
-                player_dict[turns[i]] = player_names[i]
-            break
+        try:
+            num = int(input(f"The computer selected a number between 1 to 100. {player_names[super_turn]} guess the number: "))
+            turn += 1
+            if num < random_number:
+                print(f"Try something bigger")
+            elif num > random_number:
+                print(f"Try something smaller")
+            else:
+                print(f"You took {turn} turns to guess it. great job!!\n============================================\n")
+                turns.append(turn)
+                turn = 0
+                super_turn += 1
+                for i in range(super_turn):
+                    player_dict[turns[i]] = player_names[i]
+                break
+        except ValueError:
+            print("You must enter a number")
 
 for i in range(len(turns)):
     if turns[i] == min(turns):
